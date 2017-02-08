1a9948ce60e73773688ebfa65f4eac3cacbc0a66
lejianwen <84855512@qq.com>
2017-02-08 14:04:26 +0800

    配置session为空时使用默认session

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -71,15 +71,18 @@ class bootstrap
     public static function session()
     {
         $session_driver = config('app.session');
-        $class = 'lib\session\\' . $session_driver;
-        $handler = new $class;
-        session_set_save_handler(
-            array(&$handler,"open"),
-            array(&$handler,"close"),
-            array(&$handler,"read"),
-            array(&$handler,"write"),
-            array(&$handler,"destroy"),
-            array(&$handler,"gc"));
+        if($session_driver)
+        {
+            $class = 'lib\session\\' . $session_driver;
+            $handler = new $class;
+            session_set_save_handler(
+                array(&$handler,"open"),
+                array(&$handler,"close"),
+                array(&$handler,"read"),
+                array(&$handler,"write"),
+                array(&$handler,"destroy"),
+                array(&$handler,"gc"));
+        }
         register_shutdown_function('session_write_close');
         session_write_close();
         session_start();
