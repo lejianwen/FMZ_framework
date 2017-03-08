3c1605cebefc86432f0774ce9ee427d57c62f7da
lejianwen <84855512@qq.com>
2017-03-08 11:50:01 +0800

    添加配置项

--- config/app.php
+++ config/app.php
@@ -8,7 +8,7 @@
  */
 return [
     //session存放 为空表示用自带的, '' || mysql_pdo || redis || prdis, 具体看lib/session中的文件
-    'session'          => 'mysql_pdo',
+    'session'          => '',
     'session_table'    => 'session',    //session 存放在mysql中的表名
     'session_lefttime' => 1000,         //session有效时间
     'session_redis_db' => 0,            //session 存放在redis中的某个db
@@ -16,7 +16,7 @@ return [
     'view'             => 'smarty',     //模板 smarty||native
     'smarty'           => [             //smarty配置
         'debug'           => false,     //是否弹出debug窗口
-        'force_compile'   => true,     //检查模板是否改动,开发时打开,正式 关闭
+        'force_compile'   => false,     //检查模板是否改动,开发时打开,正式 关闭
         'cache'           => false,     //是否缓存
         'cache_lifetime'  => 120,       //缓存时间
         'cache_dir'       => BASE_PATH . 'runtime/smarty/cache/',          //缓存目录
@@ -27,7 +27,8 @@ return [
     'cache'            => 'file',       //缓存类型，file||redis||predis
     'cache_expire'     => 120,          //缓存时间
     'cache_file_dir'   => BASE_PATH . 'runtime/cache/',
-    'cache_redis_db'   => 1
-
+    'cache_redis_db'   => 1,
+    'sys_log'          => true,         //系统日志是否开启
+    'sys_error_log'    => true          //系统错误日志是否开启
 
 ];
