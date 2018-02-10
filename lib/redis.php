2f48175c7ea32b541069fc20ab7d95a56e05815c
84855512@qq.com <ljw.1989>
2018-02-10 11:06:04 +0800

    更新lib\Store,  exp默认为空，永久， lib\redis添加一个有序集合操作，用于排行榜

--- lib/redis.php
+++ lib/redis.php
@@ -66,4 +66,52 @@ class redis
     {
         return $this->connect()->client->$func(...$params);
     }
+
+    /**
+     * 给有序集合添加元素，并判断是否已经超过限制
+     * 超过限制则根据是否倒序剔除多余的元素
+     * @param $column
+     * @param $score
+     * @param $id
+     * @param int $limit
+     * @param bool $desc
+     * @author Lejianwen
+     */
+    public function toAttrList($column, $score, $id, $limit = 1000, $desc = true)
+    {
+        $key = "{$column}:list";
+        redis()->zAdd($key, $score, $id);
+        $num = redis()->zCard($key);
+        if ($num >= $limit) {
+            if ($desc) {
+                //倒序删除多出来分数小的
+                $this->connect()->client->zRemRangeByRank($key, 0, ($num - $limit - 1));
+            } else {
+                $this->connect()->client->zRemRangeByRank($key, ($limit - $num), -1);
+            }
+        }
+    }
+
+    /**
+     * 根据某字段的排行榜，是个存在redis中的有序集合
+     * @param string $column 字段
+     * @param int $offset 偏移数
+     * @param int $limit 返回行数
+     * @param bool $desc 是否倒序
+     * @return array
+     * @author Lejianwen
+     */
+    public function attrList($column, $offset = 0, $limit = 5, $desc = true)
+    {
+        if ($limit <= 0) {
+            return [];
+        }
+        $offset_end = $limit - 1;
+        if ($desc) {
+            $list = $this->connect()->client->zRevRange("{$column}:list", $offset, $offset_end);
+        } else {
+            $list = $this->connect()->client->zRange("{$column}:list", $offset, $offset_end);
+        }
+        return $list;
+    }
 }
