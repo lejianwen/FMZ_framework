a0a86ae2f0d1f4e81758ae3d0c4287ddf9ef637a
84855512@qq.com <ljw.1989>
2018-02-27 16:31:21 +0800

    up form的获取方式

--- app/controllers/admin/TestController.php
+++ app/controllers/admin/TestController.php
@@ -8,9 +8,6 @@
 
 namespace app\controllers\admin;
 
-use app\controllers\admin\html\Form;
-use app\controllers\admin\html\Grid;
-
 class TestController extends BaseController
 {
     protected $search_columns = ['id', 'name'];
@@ -18,27 +15,29 @@ class TestController extends BaseController
 
     protected function form($item = null)
     {
-        $form = new Form($item);
-        $form->text('名称', 'name')->required();
-//        $form->file('附件', 'file')->isMultiple();
-        $form->image('图片', 'images')->accept('.png,.jpg');
-        $form->time('时间', 'created_at');
-        $form->time('时间', 'updated_at')->format('yyyy-MM-dd');
-        $form->select('状态', 'status')->options([
+        parent::form($item);
+        $this->form->text('名称', 'name')->required();
+//        $this->form->file('附件', 'file')->isMultiple();
+        $this->form->image('图片', 'images')->accept('.png,.jpg');
+        $this->form->time('时间', 'created_at');
+        $this->form->time('时间', 'updated_at')->format('yyyy-MM-dd');
+        $this->form->select('状态', 'status')->options([
             ['label' => '启用', 'value' => 1],
             ['label' => '禁用', 'value' => 0, 'default' => 1]
         ])->required();
-        return $form;
+
     }
 
     protected function grid()
     {
         parent::grid();
-        $this->grid->setHeader(['ID', '名称', '图片', '管理员', '状态']);
+        $this->grid->setHeader(['ID', '名称', '图片', '管理员', '创建时间', '更新时间', '状态']);
         $this->grid->text('id')->orderAble();
         $this->grid->label('name')->orderAble();
         $this->grid->img('images');
         $this->grid->text('admins.username');
+        $this->grid->text('created_at');
+        $this->grid->text('updated_at');
         $this->grid->switchLabel('status', [
             ['label' => '禁用', 'value' => 0, 'class' => 'label-grey'],
             ['label' => '启用', 'value' => 1, 'class' => 'label-success']
@@ -56,9 +55,10 @@ class TestController extends BaseController
         $this->search_form->eq('STATUS', 'status', 'select')->options([
             ['label' => '==请选择==', 'value' => null],
             ['label' => '禁用', 'value' => 0],
-            ['label' => '启用', 'value' => 1, 'default' => 1]
+            ['label' => '启用', 'value' => 1]
         ]);
-//        $this->search_form->equal(function ($search_form) {
+        $this->search_form->egt('创建时间', 'created_at', 'time');
+//        $this->search_form->eq(function ($search_form) {
 //            return $search_form->select('STATUS', 'status')->options([
 //                ['label' => '禁用', 'value' => 0, 'class' => 'label-grey'],
 //                ['label' => '启用', 'value' => 1, 'class' => 'label-success']
