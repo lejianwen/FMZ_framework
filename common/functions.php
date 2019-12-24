860ffb75f3939ad6f027e8d4d4cc50c3769f8fba
jianwen_le <84855512@qq.com>
2019-12-24 17:53:06 +0800

    update

--- common/functions.php
+++ common/functions.php
@@ -369,3 +369,28 @@ function appLog($level, $message = '', $data = [])
     $info_log->pushHandler(new \Monolog\Handler\StreamHandler(RUNTIME_PATH . 'app/' . date('Y-m-d') . '.log', $level));
     $info_log->$level($message, $data);
 }
+
+
+if (!function_exists('random_str')) {
+    /**
+     * 随机字符串
+     * @param int $length
+     * @return string
+     * @throws Exception
+     * @author lejianwen
+     */
+    function random_str($length = 16)
+    {
+        $string = '';
+
+        while (($len = strlen($string)) < $length) {
+            $size = $length - $len;
+
+            $bytes = random_bytes($size);
+
+            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+        }
+
+        return $string;
+    }
+}
