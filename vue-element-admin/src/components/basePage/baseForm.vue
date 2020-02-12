77850747e65534af4d5af8eb2e3bfde1eef6c727
jianwe_le <84855512@qq.com>
2020-02-12 14:38:58 +0800

    添加 checkboxGroup

--- vue-element-admin/src/components/basePage/baseForm.vue
+++ vue-element-admin/src/components/basePage/baseForm.vue
@@ -1,146 +1,155 @@
 <template>
   <div>
     <el-form
-      v-if="formOptions.length"
-      :ref="refId"
-      :model="currentValue"
-      label-position="right"
-      :label-width="labelWidth"
-      :rules="rules"
-      :disabled="disabled"
-      class="base-form"
+            v-if="formOptions.length"
+            :ref="refId"
+            :model="currentValue"
+            label-position="right"
+            :label-width="labelWidth"
+            :rules="rules"
+            :disabled="disabled"
+            class="base-form"
     >
       <template v-for="(formItem,index) in formOptions">
         <slot v-if="formItem.type==='slot'" :name="formItem.prop" />
         <el-form-item
-          v-else
-          :key="index"
-          :label="formItem.label"
-          :prop="formItem.prop"
-          :rules="formItem.rules && formItem.rules.length ? formItem.rules : undefined"
+                v-else
+                :key="index"
+                :label="formItem.label"
+                :prop="formItem.prop"
+                :rules="formItem.rules && formItem.rules.length ? formItem.rules : undefined"
         >
           <el-input v-if="formItem.type === 'input'" v-model="currentValue[formItem.prop]" :placeholder="formItem.placeholder" clearable :disabled="formItem.disabled||false" />
           <el-input v-if="formItem.type === 'textarea'" v-model="currentValue[formItem.prop]" :placeholder="formItem.placeholder" clearable type="textarea" :disabled="formItem.disabled||false" />
           <el-input v-if="formItem.type === 'password'" v-model="currentValue[formItem.prop]" :placeholder="formItem.placeholder" clearable type="password" :disabled="formItem.disabled||false" />
           <el-input-number v-if="formItem.type === 'number'" v-model="currentValue[formItem.prop]" :placeholder="formItem.placeholder" clearable :disabled="formItem.disabled||false" />
           <el-select
-            v-if="formItem.type === 'select'"
-            v-model="currentValue[formItem.prop]"
-            :placeholder="formItem.placeholder"
-            :multiple="formItem.multiple?true:false"
-            clearable
-            :disabled="formItem.disabled||false"
-            @change="handleSelectChange({formItem, $event})"
+                  v-if="formItem.type === 'select'"
+                  v-model="currentValue[formItem.prop]"
+                  :placeholder="formItem.placeholder"
+                  :multiple="formItem.multiple?true:false"
+                  clearable
+                  :allow-create="formItem.allowCreate||false"
+                  :disabled="formItem.disabled||false"
+                  :filterable="formItem.filterable||formItem.allowCreate||false"
+                  @change="handleSelectChange({formItem, $event})"
           >
             <el-option
-              v-for="item in formItem.options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+                    v-for="item in formItem.options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
             />
           </el-select>
 
           <el-checkbox
-            v-if="formItem.type==='checkbox'"
-            v-model="currentValue[formItem.prop]"
-            :true-label="formItem.trueLabel||1"
-            :false-label="formItem.falseLabel||0"
-            :disabled="formItem.disabled||false"
+                  v-if="formItem.type==='checkbox'"
+                  v-model="currentValue[formItem.prop]"
+                  :true-label="formItem.trueLabel||1"
+                  :false-label="formItem.falseLabel||0"
+                  :disabled="formItem.disabled||false"
           >{{ formItem.label }}
           </el-checkbox>
 
+          <template v-if="formItem.type === 'checkboxGroup'">
+            <el-checkbox v-model="checkAll[formItem.prop]" :indeterminate="isIndeterminate[formItem.prop]" @change="handleCheckAllChange({formItem, $event})">全选</el-checkbox>
+            <el-checkbox-group v-model="currentValue[formItem.prop]" @change="handleCheckGroupCheckedChange({formItem, $event})">
+              <el-checkbox v-for="item in formItem.options" :key="item.value" :label="item.value">{{ item.label }}</el-checkbox>
+            </el-checkbox-group>
+          </template>
+
           <el-cascader
-            v-if="formItem.type==='cascader'"
-            v-model="currentValue[formItem.prop]"
-            :options="formItem.options"
-            :props="formItem.props"
-            :show-all-levels="formItem.showAllLevels"
-            filterable
-            clearable
+                  v-if="formItem.type==='cascader'"
+                  v-model="currentValue[formItem.prop]"
+                  :options="formItem.options"
+                  :props="formItem.props"
+                  :show-all-levels="formItem.showAllLevels"
+                  filterable
+                  clearable
           />
           <el-switch
-            v-if="formItem.type==='switch'"
-            v-model="currentValue[formItem.prop]"
-            :active-text="formItem.activeText"
-            :inactive-text="formItem.inactiveText"
-            :active-color="formItem.activeColor||'#13ce66'"
-            :inactive-color="formItem.inactiveColor||''"
-            :active-value="formItem.activeValue||1"
-            :inactive-value="formItem.inactiveValue||0"
-            @change="handleSwitchChange({formItem, $event})"
+                  v-if="formItem.type==='switch'"
+                  v-model="currentValue[formItem.prop]"
+                  :active-text="formItem.activeText"
+                  :inactive-text="formItem.inactiveText"
+                  :active-color="formItem.activeColor||'#13ce66'"
+                  :inactive-color="formItem.inactiveColor||''"
+                  :active-value="formItem.activeValue||1"
+                  :inactive-value="formItem.inactiveValue||0"
+                  @change="handleSwitchChange({formItem, $event})"
           />
           <el-date-picker
-            v-if="formItem.type === 'date'"
-            v-model="currentValue[formItem.prop]"
-            :format="formItem.format ? formItem.format : 'yyyy-MM-dd'"
-            :value-format="formItem.vauleFormat ? formItem.vauleFormat : 'yyyy-MM-dd'"
-            type="date"
-            :placeholder="formItem.placeholder || '选择日期'"
-            :disabled="formItem.disabled||false"
+                  v-if="formItem.type === 'date'"
+                  v-model="currentValue[formItem.prop]"
+                  :format="formItem.format ? formItem.format : 'yyyy-MM-dd'"
+                  :value-format="formItem.vauleFormat ? formItem.vauleFormat : 'yyyy-MM-dd'"
+                  type="date"
+                  :placeholder="formItem.placeholder || '选择日期'"
+                  :disabled="formItem.disabled||false"
           />
           <el-date-picker
-            v-if="formItem.type === 'datetime'"
-            v-model="currentValue[formItem.prop]"
-            :format="formItem.format ? formItem.format : 'yyyy-MM-dd HH:mm:ss'"
-            :value-format="formItem.valueFormat ? formItem.valueFormat : 'yyyy-MM-dd HH:mm:ss'"
-            type="datetime"
-            :placeholder="formItem.placeholder || '选择时间'"
-            :disabled="formItem.disabled||false"
+                  v-if="formItem.type === 'datetime'"
+                  v-model="currentValue[formItem.prop]"
+                  :format="formItem.format ? formItem.format : 'yyyy-MM-dd HH:mm:ss'"
+                  :value-format="formItem.valueFormat ? formItem.valueFormat : 'yyyy-MM-dd HH:mm:ss'"
+                  type="datetime"
+                  :placeholder="formItem.placeholder || '选择时间'"
+                  :disabled="formItem.disabled||false"
           />
           <el-time-picker
-            v-if="formItem.type === 'timepicker'"
-            v-model="currentValue[formItem.prop]"
-            :placeholder="formItem.placeholder || '选择时间'"
-            :picker-options="formItem.options"
-            :disabled="formItem.disabled||false"
+                  v-if="formItem.type === 'timepicker'"
+                  v-model="currentValue[formItem.prop]"
+                  :placeholder="formItem.placeholder || '选择时间'"
+                  :picker-options="formItem.options"
+                  :disabled="formItem.disabled||false"
           />
           <el-time-select
-            v-if="formItem.type === 'timeselect'"
-            v-model="currentValue[formItem.prop]"
-            :placeholder="formItem.placeholder || '选择时间'"
-            :picker-options="formItem.options"
-            :disabled="formItem.disabled||false"
+                  v-if="formItem.type === 'timeselect'"
+                  v-model="currentValue[formItem.prop]"
+                  :placeholder="formItem.placeholder || '选择时间'"
+                  :picker-options="formItem.options"
+                  :disabled="formItem.disabled||false"
           />
 
           <el-upload
-            v-if="formItem.type === 'file'"
-            :on-success="formItem.onSuccess ? formItem.onSuccess : null"
-            :on-remove="formItem.onRemove?formItem.onRemove:()=>{}"
-            :before-upload="formItem.beforeUpload?formItem.beforeUpload:()=>{}"
-            :name="formItem.name"
-            :action="formItem.action"
-            :file-list="formItem.fileList"
-            :limit="formItem.limit?formItem.limit:0"
-            :disabled="formItem.disabled||false"
+                  v-if="formItem.type === 'file'"
+                  :on-success="formItem.onSuccess ? formItem.onSuccess : null"
+                  :on-remove="formItem.onRemove?formItem.onRemove:()=>{}"
+                  :before-upload="formItem.beforeUpload?formItem.beforeUpload:()=>{}"
+                  :name="formItem.name"
+                  :action="formItem.action"
+                  :file-list="formItem.fileList"
+                  :limit="formItem.limit?formItem.limit:0"
+                  :disabled="formItem.disabled||false"
           >
             <el-button size="small" type="primary">点击上传</el-button>
             <div v-if="formItem.tip" slot="tip" class="el-upload__tip">{{ formItem.tip }}</div>
           </el-upload>
 
           <el-upload
-            v-if="formItem.type === 'images'"
-            :on-success="formItem.onSuccess?formItem.onSuccess:()=>{}"
-            :on-remove="formItem.onRemove?formItem.onRemove:()=>{}"
-            :before-upload="formItem.beforeUpload?formItem.beforeUpload:()=>{}"
-            :name="formItem.name"
-            :action="formItem.action"
-            :file-list="formItem.fileList"
-            :limit="formItem.limit?formItem.limit:0"
-            :disabled="formItem.disabled||false"
-            list-type="picture-card"
+                  v-if="formItem.type === 'images'"
+                  :on-success="formItem.onSuccess?formItem.onSuccess:()=>{}"
+                  :on-remove="formItem.onRemove?formItem.onRemove:()=>{}"
+                  :before-upload="formItem.beforeUpload?formItem.beforeUpload:()=>{}"
+                  :name="formItem.name"
+                  :action="formItem.action"
+                  :file-list="formItem.fileList"
+                  :limit="formItem.limit?formItem.limit:0"
+                  :disabled="formItem.disabled||false"
+                  list-type="picture-card"
           >
             <i class="el-icon-plus" />
             <div v-if="formItem.tip" slot="tip" class="el-upload__tip">{{ formItem.tip }}</div>
           </el-upload>
           <el-upload
-            v-if="formItem.type === 'avatar'"
-            class="avatar-uploader"
-            :name="formItem.name"
-            :action="formItem.action"
-            :show-file-list="false"
-            :on-success="formItem.onSuccess ? formItem.onSuccess : null"
-            :before-upload="formItem.beforeUpload ? formItem.beforeUpload : null"
-            :disabled="formItem.disabled||false"
+                  v-if="formItem.type === 'avatar'"
+                  class="avatar-uploader"
+                  :name="formItem.name"
+                  :action="formItem.action"
+                  :show-file-list="false"
+                  :on-success="formItem.onSuccess ? formItem.onSuccess : null"
+                  :before-upload="formItem.beforeUpload ? formItem.beforeUpload : null"
+                  :disabled="formItem.disabled||false"
           >
             <img v-if="currentValue[formItem.prop]" :src="currentValue[formItem.prop]" class="avatar">
             <i v-else class="el-icon-plus avatar-uploader-icon" />
@@ -167,83 +176,110 @@
 </template>
 
 <script>
-import Tinymce from '@/components/Tinymce'
-export default {
-  name: 'BaseForm',
-  components: { Tinymce },
-  props: {
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    refId: {
-      type: String,
-      default: 'baseForm'
-    },
-    rules: {
-      type: Object,
-      default() {
-        return {}
-      }
-    },
-    createBtn: {
-      type: Boolean,
-      default: true
-    },
-    labelWidth: {
-      type: String,
-      default: '120px'
-    },
-    value: {
-      type: Object,
-      default() {
-        return {}
-      }
-    },
-    formOptions: {
-      type: Array,
-      default() {
-        return []
-      }
-    }
-  },
-  data() {
-    return {
-      headimgs: []
-    }
-  },
-  computed: {
-    currentValue: {
-      get() {
-        return this.value
+  import Tinymce from '@/components/Tinymce'
+  export default {
+    name: 'BaseForm',
+    components: { Tinymce },
+    props: {
+      disabled: {
+        type: Boolean,
+        default: false
       },
-      set(val) {
-        this.$emit('update:value', val)
+      refId: {
+        type: String,
+        default: 'baseForm'
+      },
+      rules: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      createBtn: {
+        type: Boolean,
+        default: true
+      },
+      labelWidth: {
+        type: String,
+        default: '120px'
+      },
+      value: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      formOptions: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        headimgs: [],
+        isIndeterminate: [],
+        checkAll: []
+      }
+    },
+    computed: {
+      currentValue: {
+        get() {
+          return this.value
+        },
+        set(val) {
+          this.$emit('update:value', val)
+        }
+      }
+    },
+    created() {
+    },
+    methods: {
+      init() {
+        this.formOptions.forEach(formItem => {
+          if (formItem.type === 'checkboxGroup') {
+            this.isIndeterminate[formItem.prop] = ((this.currentValue[formItem.prop].length > 0) && (this.currentValue[formItem.prop].length < formItem.options.length))
+            this.checkAll[formItem.prop] = (this.currentValue[formItem.prop].length === formItem.options.length)
+            this.checkAll = Object.assign({}, this.checkAll)
+            this.isIndeterminate = Object.assign({}, this.isIndeterminate)
+          }
+        })
+      },
+      handleCancel() {
+        this.$emit('handleCancel')
+      },
+      handleSubmit() {
+        let res = false
+        this.$refs[this.refId].validate((valid) => {
+          res = valid
+        })
+        this.$emit('handleSubmit', res)
+      },
+      handleSelectChange({ formItem, $event }) {
+        this.$emit('handleSelectChange', { formItem, $event })
+      },
+      handleSwitchChange({ formItem, $event }) {
+        this.$emit('handleSwitchChange', { formItem, $event })
+      },
+      handleCheckAllChange({ formItem, $event }) {
+        this.checkAll[formItem.prop] = $event
+        this.checkAll = Object.assign({}, this.checkAll)
+        this.currentValue[formItem.prop] = $event ? formItem.options.map(item => item.value) : []
+        this.isIndeterminate[formItem.prop] = false
+        this.$emit('handleCheckAllChange', { formItem, $event })
+      },
+
+      handleCheckGroupCheckedChange({ formItem, $event }) {
+        this.checkAll[formItem.prop] = (this.currentValue[formItem.prop].length === formItem.options.length)
+        this.checkAll = Object.assign({}, this.checkAll)
+        this.isIndeterminate[formItem.prop] = ((this.currentValue[formItem.prop].length > 0) && (this.currentValue[formItem.prop].length < formItem.options.length))
+        this.isIndeterminate = Object.assign({}, this.isIndeterminate)
+        this.$emit('handleCheckGroupCheckedChange', { formItem, $event })
       }
     }
-  },
-  created() {
-  },
-  methods: {
-    handleCancel() {
-      this.$emit('handleCancel')
-    },
-    handleSubmit() {
-      let res = false
-      this.$refs[this.refId].validate((valid) => {
-        res = valid
-      })
-      this.$emit('handleSubmit', res)
-    },
-    handleSelectChange({ formItem, $event }) {
-      this.$emit('handleSelectChange', { formItem, $event })
-    },
-    handleSwitchChange({ formItem, $event }) {
-      this.$emit('handleSwitchChange', { formItem, $event })
-    }
-  }
 
-}
+  }
 </script>
 
 <style>
