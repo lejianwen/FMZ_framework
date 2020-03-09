5c95b13e1f00f0d6f7d460c41035700bdf57026a
lejianwen <826329935@qq.com>
2020-03-09 16:06:22 +0800

    up

--- app/models/Config.php
+++ app/models/Config.php
@@ -47,10 +47,10 @@ class Config extends Model
      *     definition="Config",
      *     type="object",
      *     @SWG\Property(property="id",type="number", description="id"),
-     *     @SWG\Property(property="name",type="string", description="名称",example="网站名"),
-     *     @SWG\Property(property="code",type="string", description="配置码",example="SITE_NAME"),
-     *     @SWG\Property(property="value",type="string", description="配置值",example="淘宝客网站"),
-     *     @SWG\Property(property="status",type="number", description="状态 1启用， 2 禁用",example="1"),
+     *     @SWG\Property(property="name",type="string", description="名称",example="52FMZ"),
+     *     @SWG\Property(property="code",type="string", description="配置码",example="YOUR_CODE"),
+     *     @SWG\Property(property="value",type="string", description="配置值",example="我的码呀"),
+     *     @SWG\Property(property="status",type="number", description="状态 1启用， 2 禁用",example="1", enum={1,2}),
      *     @SWG\Property(property="created_at",type="string", description="创建时间", example="2019-10-10 10:11:12"),
      *     @SWG\Property(property="updated_at",type="string", description="更新时间", example="2019-10-10 10:11:12")
      * )
