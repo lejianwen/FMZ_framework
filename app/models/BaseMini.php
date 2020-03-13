cb9a7f455719c3f04e7539d7a9fc01773bbb608d
lejianwen <826329935@qq.com>
2020-03-13 17:47:05 +0800

    upgrade

---
+++ app/models/BaseMini.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Class BaseMini
+ * Author lejianwen
+ * Date: 2020/3/13 17:25
+ */
+
+namespace app\models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseMini extends Model
+{
+    protected $guarded = [];
+    const CACHE_ALL_KEY = '';
+
+    public static function allInfo()
+    {
+        static $list;
+        if (!empty($list)) {
+            return $list;
+        }
+        $list = cache()->get(static::CACHE_ALL_KEY);
+        if (!empty($list)) {
+            return $list;
+        }
+        $list = static::query()->where('status', COMMON_STATUS_ENABLE)->orderByDesc('id')->get()->toArray();
+        cache()->set(static::CACHE_ALL_KEY, $list);
+        return $list;
+    }
+
+    public static function findByColumn($column, $value)
+    {
+        $list = static::allInfo();
+        foreach ($list as $key => $item) {
+            if ($item[$column] == $value) {
+                return $item;
+            }
+        }
+        return [];
+    }
+}
--- app/models/Config.php
+++ app/models/Config.php
@@ -2,57 +2,28 @@
 
 namespace app\models;
 
-use Illuminate\Database\Eloquent\Model;
 
-class Config extends Model
+class Config extends BaseMini
 {
-    protected $guarded = [];
     const CACHE_ALL_KEY = 'all:config';
-
-    public static function allInfo()
-    {
-        static $list;
-        if (!empty($list)) {
-            return $list;
-        }
-        $list = cache()->get(self::CACHE_ALL_KEY);
-        if (!empty($list)) {
-            return $list;
-        }
-        $list = self::query()->where('status', COMMON_STATUS_ENABLE)->orderByDesc('id')->get()->toArray();
-        cache()->set(self::CACHE_ALL_KEY, $list);
-        return $list;
-    }
 
     public static function findByCode($code)
     {
         $item = self::findByColumn('code', $code);
         return $item ? $item['value'] : null;
     }
-
-    public static function findByColumn($column, $value)
-    {
-        $list = self::allInfo();
-        foreach ($list as $key => $item) {
-            if ($item[$column] == $value) {
-                return $item;
-            }
-        }
-        return [];
-    }
-
-
-    /**
-     * @SWG\Definition(
-     *     definition="Config",
-     *     type="object",
-     *     @SWG\Property(property="id",type="number", description="id"),
-     *     @SWG\Property(property="name",type="string", description="名称",example="52FMZ"),
-     *     @SWG\Property(property="code",type="string", description="配置码",example="YOUR_CODE"),
-     *     @SWG\Property(property="value",type="string", description="配置值",example="我的码呀"),
-     *     @SWG\Property(property="status",type="number", description="状态 1启用， 2 禁用",example="1", enum={1,2}),
-     *     @SWG\Property(property="created_at",type="string", description="创建时间", example="2019-10-10 10:11:12"),
-     *     @SWG\Property(property="updated_at",type="string", description="更新时间", example="2019-10-10 10:11:12")
-     * )
-     */
 }
+
+/**
+ * @SWG\Definition(
+ *     definition="Config",
+ *     type="object",
+ *     @SWG\Property(property="id",type="number", description="id"),
+ *     @SWG\Property(property="name",type="string", description="名称",example="网站名"),
+ *     @SWG\Property(property="code",type="string", description="配置码",example="SITE_NAME"),
+ *     @SWG\Property(property="value",type="string", description="配置值",example="淘宝客网站"),
+ *     @SWG\Property(property="status",type="number", description="状态 1启用， 2 禁用",example="1"),
+ *     @SWG\Property(property="created_at",type="string", description="创建时间", example="2019-10-10 10:11:12"),
+ *     @SWG\Property(property="updated_at",type="string", description="更新时间", example="2019-10-10 10:11:12")
+ * )
+ */
